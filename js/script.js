33ad51faf2102737cff36592a879d4fd552e0506
max221088 <117590988+max221088@users.noreply.github.com>
2023-02-06 00:03:09 +0200

    add side menu and btn exit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const $btnMenu = document.querySelector(".menu-btn");
 const $contArea = document.querySelector(".content_wrapper");
+const $btnExit = document.querySelector(".exit_btn");
+const $navMenu = document.querySelector(".nav_menu");
 
 $btnMenu.addEventListener("click", function () {
-	$contArea.classList.toggle("active");
+	$contArea.classList.add("active");
+	$navMenu.classList.add("active");
+});
+$btnExit.addEventListener("click", function () {
+	$contArea.classList.remove("active");
+	$navMenu.classList.remove("active");
 });
