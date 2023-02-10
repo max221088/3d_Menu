04048603ad61b0a6f48501664bafd900206eb5f7
max221088 <117590988+max221088@users.noreply.github.com>
2023-02-10 22:45:05 +0200

    finished menu rout

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Vars
 const $btnMenu = document.querySelector(".menu-btn");
 const $contArea = document.querySelector(".content_wrapper");
 const $btnExit = document.querySelector(".exit_btn");
@@ -6,21 +7,42 @@ const $menuItems = document.querySelectorAll(".items");
 const $innerCont = document.querySelector(".inner_content");
 const routes = {
 	404: "/pages/404.html",
-	"/": "/index.html",
-	"/about": "/pages/about.html",
-	"/contact": "/pages/contact.html",
-	"/gallery": "/pages/gallery.html",
-	"/partners": "/pages/partners.html",
+	"#/": "/index.html",
+	"#/about": "/pages/about.html",
+	"#/contact": "/pages/contact.html",
+	"#/gallery": "/pages/gallery.html",
+	"#/partners": "/pages/partners.html",
 };
 
-function exit() {
-	$contArea.classList.remove("active");
-	$navMenu.classList.remove("active");
-}
+// Controllers
+
 function active() {
 	$contArea.classList.add("active");
 	$navMenu.classList.add("active");
 }
+function exit() {
+	$contArea.classList.remove("active");
+	$navMenu.classList.remove("active");
+}
+function liveReload(link) {
+	let route = routes[link];
+	fetch(route)
+		.then(function (response) {
+			return response.text();
+		})
+		.then(function (data) {
+			let tamplate = data;
+			exit();
+			$innerCont.innerHTML = tamplate;
+		});
+}
+
+// Innit section
+
+history.pushState(null, null, routes["#/"]);
+location.hash = "#/";
+
+// Actions
 
 $btnMenu.addEventListener("click", active);
 
@@ -28,17 +50,12 @@ $btnExit.addEventListener("click", exit);
 
 $menuItems.forEach(function (el) {
 	el.addEventListener("click", function () {
-		let rout = this.getAttribute("data-link");
-		console.log(rout);
-		fetch(rout)
-			.then(function (response) {
-				return response.text();
-			})
-			.then(function (data) {
-				let tamplate = data;
-				$innerCont.innerHTML = tamplate;
-				exit();
-				console.log(tamplate);
-			});
+		let link = this.getAttribute("data-link");
+		location.hash = link;
+		//liveReload(link);
 	});
+});
+
+window.addEventListener("hashchange", function () {
+	liveReload(window.location.hash);
 });
